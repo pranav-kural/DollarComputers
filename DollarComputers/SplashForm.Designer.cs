34aab6b5dbcc86521642ed968a11135ff728f2b4
Pranav Kural <pranavkural042@gmail.com>
2017-03-13 17:06:58 -0400

    Created the splash form

---
+++ DollarComputers/SplashForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace DollarComputers
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DollarComputersLabel = new System.Windows.Forms.Label();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DollarComputersLabel
+            // 
+            this.DollarComputersLabel.AutoSize = true;
+            this.DollarComputersLabel.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DollarComputersLabel.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.DollarComputersLabel.Location = new System.Drawing.Point(325, 42);
+            this.DollarComputersLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.DollarComputersLabel.Name = "DollarComputersLabel";
+            this.DollarComputersLabel.Size = new System.Drawing.Size(224, 33);
+            this.DollarComputersLabel.TabIndex = 0;
+            this.DollarComputersLabel.Text = "ollar Computers";
+            this.DollarComputersLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.BackgroundImage = global::DollarComputers.Properties.Resources.logo_img;
+            this.LogoPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LogoPictureBox.Location = new System.Drawing.Point(258, 12);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(103, 82);
+            this.LogoPictureBox.TabIndex = 1;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::DollarComputers.Properties.Resources.intro_hero;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 328);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 405);
+            this.ControlBox = false;
+            this.Controls.Add(this.DollarComputersLabel);
+            this.Controls.Add(this.LogoPictureBox);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.ShowIcon = false;
+            this.Text = "Dollar Computers";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DollarComputersLabel;
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DollarComputers/SplashForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DollarComputers
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
